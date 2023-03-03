c0c95024e136a5f12d8aa8a5d721c5d1057609ca
Ann Dolbina <dolbinaann@gmail.com>
2023-03-03 23:03:33 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+  // Знаходимо елементи: поля форми та кнопку
+const refs = {
+form: document.querySelector(".form"),
+delay: document.querySelector('[name="delay"]'),
+step: document.querySelector('[name="step"]'),
+amount: document.querySelector('[name = "amount"]'),
+submit: document.querySelector(".form button"),
 }
+
+// Додаємо слухача до форми
+refs.form.addEventListener('submit', onSubmit);
+
+// Колбек-функція
+function onSubmit(event) {
+ event.preventDefault();
+ const amountValue = refs.amount.value;
+const delayValue = refs.delay.value;
+  const stepValue = refs.step.value;
+  console.log(stepValue);
+   
+}
+
+
+refs.submit.addEventListener('click', onClick);
+function onClick(event) {
+  
+}
+
+
+
+
